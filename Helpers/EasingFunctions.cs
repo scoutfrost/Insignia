8801c044a2eca72f942b6a7de2bac12e087165ff
scoutfrost <125180429+scoutfrost@users.noreply.github.com>
2023-09-10 15:29:30 -0700

    slerp works

--- Helpers/EasingFunctions.cs
+++ Helpers/EasingFunctions.cs
@@ -10,9 +10,11 @@ namespace Insignia.Helpers
 {
     internal static class EasingFunctions
     {
-        public static Vector2 Slerp(Vector2 start, Vector2 end, float t)
+        public static Vector2 Slerp(Vector2 start, Vector2 end, float t, float radius = default)
         {
-            return new Vector2((float)Math.Cos(t), (float)Math.Sin(t));
+            return radius == default ? (end - start).DirectionTo(start.RotatedBy(t, end - start)) : (end - start).DirectionTo(start.RotatedBy(t, end - start)) * radius;
+            //return Utils.AngleLerp(start.ToRotation(), end.ToRotation(), t).ToRotationVector2(); alt implementations
+            // return new((float)Math.Cos(t), (float)Math.Sin(t)); //* radius; 
         }
     }
 }

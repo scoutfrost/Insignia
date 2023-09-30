3e46e38bd2ec5631085f6cb3c598e0d53b0c3b46
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-09-30 12:59:47 +1000

    Necromancy totems

---
+++ Content/Tiles/SkullAmbientTile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ObjectData;
+using Terraria.Localization;
+using Terraria.ID;
+using Terraria.Audio;
+using Insignia.Core.Common.Systems;
+namespace Insignia.Content.Tiles
+{
+    public class SkullAmbientTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileCut[Type] = false;
+            Main.tileLavaDeath[Type] = true;
+            HitSound = SoundID.Dig;
+            DustType = DustID.Stone;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+            TileObjectData.newTile.Height = 8;
+            TileObjectData.newTile.Width = 2;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16, 16, 16, 16, 16 };
+            TileObjectData.addTile(Type);
+            
+            LocalizedText name = CreateMapEntryName();
+            AddMapEntry(new Color(80, 150, 220), name);
+        }
+
+        
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Tile tile = Framing.GetTileSafely(i, j);
+
+          
+            if (Main.netMode != NetmodeID.Server)
+            {
+                SoundEngine.PlaySound(SoundID.Dig);
+
+            }
+        }
+    }
+}
